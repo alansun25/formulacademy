555b8a9fb5998b3dd1fe2e56a01e65ded4e2797b
Alan Sun <mralansun@gmail.com>
2020-07-12 01:25:10 -0400

    inflation complete

---
+++ formulas/js/inflationRate.js
@@ -0,0 +1,13 @@
+function inflationRate() {
+  var i = document.getElementById("Box1").value;
+  var j = document.getElementById("Box2").value;
+  if (i.trim() != '' && j.trim() != '') {
+    var ans = (j-i)/i*100;
+  ans= ans + " %";
+ } else {
+    answer = "Error: You must enter values for two variables.";
+  }
+  document.getElementById('answer').textContent = ans;
+  document.getElementById('Box1').value = '';
+  document.getElementById('Box2').value='';
+}
