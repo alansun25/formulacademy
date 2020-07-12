e273472cf01aabcbb8d887911ded7b6c4a82ea45
Alan Sun <mralansun@gmail.com>
2020-07-12 02:23:17 -0400

    unemployment complete

---
+++ formulas/js/unemployment.js
@@ -0,0 +1,14 @@
+function unemploymentRate() {
+  var i = document.getElementById("Box1").value;
+  var j = document.getElementById("Box2").value;
+  if (i.trim() != '' && j.trim() != '') {
+    var ans = (i/j)*100;
+    ans = parseFloat(ans).toFixed(3);
+    ans = ans + "%"
+ } else {
+    answer = "Error: You must enter values for two variables.";
+  }
+  document.getElementById('answer').textContent = ans;
+  document.getElementById('Box1').value = '';
+  document.getElementById('Box2').value='';
+}
